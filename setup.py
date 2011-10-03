251fe6c85417991ac402b4775904d22918871727
Serge Matveenko <s@matveenko.ru>
2011-10-03 14:46:57 +0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     install_requires=['distribute', 'Django<1.4'],
     name='django-supergeneric',
-    version='0.1-dev',
+    version='1.0',
     description='django-supergeneric is a small utility library that provides generic aggregator for five most used generic views for any model',
     author='Serge Matveenko',
     author_email='s@matveenko.ru',
